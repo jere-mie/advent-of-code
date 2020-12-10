b0f1273de8f37effdd380cf6859f48673794912c
jere-mie <jere-mie@users.noreply.github.com>
2020-12-10 03:10:27 -0500

    day7 p2 done. Much easier than I thought

--- day7/p2.py
+++ day7/p2.py
@@ -1,14 +1,16 @@
-"""
-# PSEUDOCODE FOR SOLUTION
-
-def check(bag a):
-    sum = 0
-    for child(num, colour) in bag:
-        sum+=num*check(colour)
+import re
+lines = []
+relats = dict()
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+for i in lines:
+    bag = re.findall(r'(\w+ \w+)(?: bags contain )', i)
+    relats[bag[0]] = []
+    if 'no' not in i:
+        relats[bag[0]] = re.findall(r'(\d+) (\w+ \w+)', i)
+def check(colour):
+    sum=0
+    for child in relats[colour]:
+        sum+= int(child[0])+int(child[0])*check(child[1])
     return sum
-
-__main__:
-    get input
-    print(check('shiny gold'))
-
-"""
+print(check('shiny gold'))
