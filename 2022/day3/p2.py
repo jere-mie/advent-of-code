ff832488d41813442c8a556cfdf0be1a158d4f67
jere-mie <jeremiedevelops@gmail.com>
2022-12-03 00:36:00 -0500

    even worse part 2 day 3

--- 2022/day3/p2.py
+++ 2022/day3/p2.py
@@ -1,4 +1,2 @@
-
-with open('input.txt', 'r') as f:
-    text = f.read().split('\n')
-    print(sum(map(lambda x: ord(x)-96 if x >= 'a' and x <= 'z' else ord(x)-38, [list(set(text[i]).intersection(set(text[i+1])).intersection(set(text[i+2])))[0] for i in range(0, len(text), 3)])))
+text = open('input.txt', 'r').read().split('\n')
+print(sum(map(lambda x: ord(x)-96 if x >= 'a' and x <= 'z' else ord(x)-38, [list(set(text[i]).intersection(set(text[i+1])).intersection(set(text[i+2])))[0] for i in range(0, len(text), 3)])))
