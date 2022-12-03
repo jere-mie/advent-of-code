d249cdd8ef64885899335e57a5d125f4530996a5
jere-mie <jeremiedevelops@gmail.com>
2022-12-03 00:37:40 -0500

    one line solution

--- 2022/day3/p2.py
+++ 2022/day3/p2.py
@@ -1,2 +1,1 @@
-text = open('input.txt', 'r').read().split('\n')
-print(sum(map(lambda x: ord(x)-96 if x >= 'a' and x <= 'z' else ord(x)-38, [list(set(text[i]).intersection(set(text[i+1])).intersection(set(text[i+2])))[0] for i in range(0, len(text), 3)])))
+print(sum(map(lambda x: ord(x)-96 if x >= 'a' and x <= 'z' else ord(x)-38, [list(set(open('input.txt', 'r').read().split('\n')[i]).intersection(set(open('input.txt', 'r').read().split('\n')[i+1])).intersection(set(open('input.txt', 'r').read().split('\n')[i+2])))[0] for i in range(0, len(open('input.txt', 'r').read().split('\n')), 3)])))
