a18a452489d836b8cfe943e091b679fde22e617c
jere-mie <jeremiedevelops@gmail.com>
2022-12-06 02:40:29 -0500

    2015 day 4

---
+++ 2015/day04/p2.py
@@ -0,0 +1,12 @@
+import hashlib
+
+inpFile = 'input.txt'
+with open(inpFile, 'r') as f:
+    text = f.read().strip()
+i = 1
+while True:
+    res = hashlib.md5(f"{text}{i}".encode()).hexdigest()
+    if res[:6] == '000000':
+        print(i)
+        exit()
+    i+=1
