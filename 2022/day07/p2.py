031e9ffb9115a5ba9e7d28bd7522e242ed570e00
jere-mie <jeremiedevelops@gmail.com>
2022-12-07 01:45:14 -0500

    I can't believe this worked...

--- 2022/day07/p2.py
+++ 2022/day07/p2.py
@@ -10,12 +10,13 @@ text = [list(filter(lambda x:x!='', i)) for i in text]
 text = list(filter(lambda x: x!=[], text))
 
 tot = 0
-
+dirTotals = []
 def processFolder(fname, index):
     print(fname)
     # find first occurence of the folder
     curr = 0
     global tot
+    global dirTotals
     for i in range(index, len(text)):
         if text[i][0] == fname:
             print('found', fname, i)
@@ -29,8 +30,16 @@ def processFolder(fname, index):
             if curr <= 100000:
                 tot+=curr
             text[i][0] = secrets.token_hex(13)
+            dirTotals.append(curr)
             return curr
     return 0
 
-print(processFolder('/', 0))
-print(tot)
+total = processFolder('/', 0)
+objective = 30000000-(70000000-total)
+print(objective)
+dirTotals = sorted(dirTotals)
+print(dirTotals)
+for i in dirTotals:
+    if i>=objective:
+        print(i)
+        exit()
