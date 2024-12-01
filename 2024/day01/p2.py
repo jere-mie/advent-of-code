8a1cd30286b1a0ef9d4edf06fa6a884efb2c03a5
Jeremie Bornais <jeremiedevelops@gmail.com>
2024-12-01 05:14:31 +0000

    2024 01 done

--- 2024/day01/p2.py
+++ 2024/day01/p2.py
@@ -8,11 +8,13 @@ for line in lines:
     nums = line.split("   ")
     left.append(int(nums[0]))
     right.append(int(nums[1]))
-left.sort()
-right.sort()
 
-distances = []
-for i in range(len(left)):
-    distances.append(abs(left[i]-right[i]))
+similarity = 0
+rightcount = dict()
+for i in right:
+    rightcount[i] = rightcount.get(i, 0) + 1
 
-print(sum(distances))
+for i in left:
+    count = rightcount.get(i, 0)
+    similarity += i*count
+print(similarity)
